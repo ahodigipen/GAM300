7790b289eb8b45d5e519a8ca043036950bc2970e
Mos <148404931+ahodigipen@users.noreply.github.com>
2025-04-23 18:34:22 +0800

    Organisation Changes

--- Gam300/Engine/BoomEngine/src/BoomEngine.cpp
+++ Gam300/Engine/BoomEngine/src/BoomEngine.cpp
@@ -1,9 +1,9 @@
 // BoomEngine.cpp : Defines the functions for the static library.
 //
 
-#include "pch.h"
-#include "framework.h"
 #include "BoomEngine.h"
+#include "Core.h"
+#include "framework.h"
 #include <iostream>
 using namespace std;
 // TODO: This is an example of a library function
