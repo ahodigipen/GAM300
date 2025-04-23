3ad5a6dccf30bbb35d0320860ecc1fb4f5ed0d42
Mos <148404931+ahodigipen@users.noreply.github.com>
2025-04-23 18:12:06 +0800

    Changes to project settings
    Small changes to project properties
    
    Updated Properties
    
    Set to all solutions:
    Warning level 4
    All warnings treated as error
    
    Organisation Changes

--- Gam300/Engine/BoomEngine/src/BoomEngine.cpp
+++ Gam300/Engine/BoomEngine/src/BoomEngine.cpp
@@ -1,9 +1,9 @@
 // BoomEngine.cpp : Defines the functions for the static library.
 //
-
-#include "pch.h"
-#include "framework.h"
+//Build BoomEngine before running debuger or building Gam300
 #include "BoomEngine.h"
+#include "Core.h"
+#include "framework.h"
 #include <iostream>
 using namespace std;
 // TODO: This is an example of a library function
