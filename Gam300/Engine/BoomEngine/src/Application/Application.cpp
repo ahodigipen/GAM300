f6153954985f1d42c2cd89c62e98ad765c42cc13
Darius <d.chan@digipen.edu>
2025-11-26 09:54:10 +0800

    hdr drag and drop support

--- Gam300/Engine/BoomEngine/src/Application/Application.cpp
+++ Gam300/Engine/BoomEngine/src/Application/Application.cpp
@@ -81,6 +81,7 @@ namespace Boom
         EnttView<Entity, SkyboxComponent>([this](auto, auto& comp) {
             SkyboxAsset& skybox{ m_Context->assets->Get<SkyboxAsset>(comp.skyboxID) };
             m_Context->renderer->InitSkybox(skybox.data, skybox.envMap, skybox.size);
+            return; //should stop after one skybox rendered
             });
 
         m_DebugLinesShader = std::make_unique<Boom::DebugLinesShader>("debug_lines.glsl");
@@ -303,8 +304,21 @@ namespace Boom
             //skybox ecs (should be drawn at the end)
             EnttView<Entity, SkyboxComponent>([this](auto entity, SkyboxComponent& comp) {
                 Transform3D& transform{ entity.template Get<TransformComponent>().transform };
+                static AssetID prevSkyID{ comp.skyboxID };
+
+                //reinitialize skybox if different
+                if (prevSkyID != comp.skyboxID) {
+                    EnttView<Entity, SkyboxComponent>([this](auto, auto& comp) {
+                        SkyboxAsset& skybox{ m_Context->assets->Get<SkyboxAsset>(comp.skyboxID) };
+                        m_Context->renderer->InitSkybox(skybox.data, skybox.envMap, skybox.size);
+                        return; //should stop after one skybox rendered
+                        });
+                }
+
+                prevSkyID = comp.skyboxID;
                 SkyboxAsset& skybox{ m_Context->assets->Get<SkyboxAsset>(comp.skyboxID) };
                 m_Context->renderer->DrawSkybox(skybox.data, transform);
+                return; //should stop after one skybox rendered
                 });
 
             m_Context->profiler.Start("Renderer End Frame");
